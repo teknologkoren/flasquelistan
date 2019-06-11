78f149216042b1762be15a0b691454290a12557d
Oskar Segersvärd <victorystick@gmail.com>
2019-06-11 22:33:39 +0200

    Fix failing tests and remove out-dated ones

--- tests/flasquelistan_tests.py
+++ tests/flasquelistan_tests.py
@@ -1,14 +1,19 @@
-import os
 import tempfile
+
 import pytest
-from flasquelistan import factory, models
+from flask import url_for
+from flask_login import current_user
+
+from flasquelistan import factory, forms, models
 
 
 @pytest.fixture
 def app():
-    db_fd, db_path = tempfile.mkstemp()
     config = {
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///' + db_path,
+        # Use an in-memory database for faster test execution.
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
+         # Disable CSRF in unit tests.
+        'WTF_CSRF_ENABLED': False,
         'TESTING': True,
     }
 
@@ -16,22 +21,67 @@ def app():
     with app.app_context():
         yield app
 
-    os.close(db_fd)
-    os.unlink(db_path)
-
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
 
-def test_empty_db(client):
-    """Test with blank database"""
-    rv = client.get('/')
-    assert b'Strequelistan' in rv.data
+def login(client, email, password):
+    return client.post('/login', data=dict(
+         email=email, password=password
+    ))
 
-    rv = client.get('/citat')
-    assert b'No quotes yet!' in rv.data
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
+
+
+def test_must_login_redirect(client):
+    """Tests that users get redirected to the login page"""
+    rv = client.get('/')
+
+    assert rv.status_code == 302
+    assert rv.headers['Location'] == 'http://localhost/login?next=%2F'
+
+def test_successful_login(app):
+    user = models.User(
+            email='monty@python.tld',
+            first_name='Monty',
+            last_name='Python',
+    )
+
+    models.db.session.add(user)
+    models.db.session.commit()
+
+    user.password = 'solidsnake'
+    models.db.session.commit()
+
+    with app.test_client() as client:
+        rv = login(client, 'monty@python.tld', 'solidsnake')
+        assert rv.status_code == 302
+        assert rv.headers['Location'] == 'http://localhost/'
+
+def test_empty_quotes(app):
+    """Test with blank database"""
+    user = models.User(
+            email='monty@python.tld',
+            first_name='Monty',
+            last_name='Python',
+    )
+
+    models.db.session.add(user)
+    models.db.session.commit()
+
+    user.password = 'solidsnake'
+    models.db.session.commit()
+
+    with app.test_client() as client:
+        rv = login(client, 'monty@python.tld', 'solidsnake')
+        assert rv.status_code == 302
+
+        rv = client.get(url_for('quotes.index'))
+        assert b'No quotes yet!' in rv.data
 
 
 def test_user_model(app):
@@ -55,90 +105,3 @@ def test_streque_model(app):
     models.db.session.commit()
 
     assert streque.id > 0
-
-
-def test_index_page(app, client):
-    import json
-
-    monty = models.User(
-        email='monty@python.tld',
-        first_name='Monty',
-        last_name='Python',
-        phone='0700011223',
-        balance=10000,
-    )
-
-    rick = models.User(
-        email='rick_astley@domain.tld',
-        first_name='Rick',
-        nickname='Roll',
-        last_name='Astley',
-        phone='0703322110',
-    )
-
-    streque = models.Streque(value=400)
-
-    models.db.session.add_all([monty, rick, streque])
-    models.db.session.commit()
-
-    rv = client.get('/')
-    assert b'Monty' in rv.data
-    assert b'Roll' in rv.data
-
-    rv = client.post('/strequa',
-                     data=json.dumps(dict(user_id=monty.id, amount=4)),
-                     content_type='application/json')
-
-    data = json.loads(rv.data)
-    assert data['user_id'] == monty.id
-    assert data['amount'] == 4
-    assert data['balance'] == 8400
-    assert monty.balance == 8400
-
-    rv = client.post('/strequa',
-                     data=json.dumps(dict(user_id=rick.id, amount=1)),
-                     content_type='application/json')
-
-    data = json.loads(rv.data)
-    assert data['user_id'] == rick.id
-    assert data['amount'] == 1
-    assert data['balance'] == -400
-    assert rick.balance == -400
-
-
-def test_strequa(app):
-    import datetime
-
-    streque = models.Streque(value=400)
-    user = models.User(email='monty@python.tld', balance=10000)
-    models.db.session.add(streque)
-    models.db.session.add(user)
-    models.db.session.commit()
-
-    time_before = datetime.datetime.utcnow()
-    transaction = user.strequa(3)
-    time_after = datetime.datetime.utcnow()
-
-    assert transaction.amount == 3
-    assert transaction.value == 400
-    assert transaction.user == user
-    assert time_after > transaction.timestamp > time_before
-
-    assert user.balance == 8800
-    assert user.transactions[0] == transaction
-
-
-def test_void(app):
-    streque = models.Streque(value=400)
-    user = models.User(email='monty@python.tld', balance=10000)
-    models.db.session.add_all([streque, user])
-    models.db.session.commit()
-
-    transaction = user.strequa(4)
-
-    assert user.balance == 8400
-
-    transaction.void_and_refund()
-
-    assert user.balance == 10000
-    assert len(models.Transaction.query.all()) == 0
