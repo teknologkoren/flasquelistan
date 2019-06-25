f1eb1a49f5c31ec6035c75da17f03808344e99ea
Martin Alge <martin@alge.se>
2019-06-25 19:35:50 +0200

    Allow for using _() and _l() in python files for translations

--- update_translations.sh
+++ update_translations.sh
@@ -1,4 +1,4 @@
 cd flasquelistan
-pybabel extract -F babel.cfg -o messages.pot .
+pybabel extract -F babel.cfg -k _l -k _ -o messages.pot .
 pybabel update -i messages.pot -d translations
 pybabel compile -d translations
