dc7776ce2df462554fe1bdb528231209a2f5e8dd
Rupus Reinefjord <rupus@reinefjord.net>
2019-11-29 23:58:02 +0100

    Fix CSP block of bulktransactionMessage.js + move scripts to head, defer
    Also, lots of style fixes.

--- flasquelistan/static/js/bulktransactionMessage.js
+++ flasquelistan/static/js/bulktransactionMessage.js
@@ -1,14 +1,14 @@
-
 function setMessage() {
-  var message = document.getElementById("bulktransactionMessage").value;
-  console.log(message);
+  var message = document.getElementById("bulktransaction-message").value;
 
   var inputs = document.getElementsByTagName("input");
 
-  for (i = 0; i < inputs.length; i++){
-    if (inputs[i].type=="text"){
+  for (i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == "text") {
       inputs[i].value = message
     }
   }
 }
 
+var button = document.getElementById("set-bulktransaction-message");
+button.addEventListener('click', setMessage, false);
