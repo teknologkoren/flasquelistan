d1b7d25d4fe13d683d7f077787bfb2330b7c33c9
Rupus Reinefjord <rupus@reinefjord.net>
2021-06-13 11:58:22 +0200

    Upgrade deps, including Flask 2.0 et. al, sqlalchemy 1.4

--- flasquelistan/views/serviceworker.py
+++ flasquelistan/views/serviceworker.py
@@ -4,6 +4,6 @@ mod = flask.Blueprint('serviceworker', __name__)
 
 @mod.route('/sw.js')
 def serviceworker():
-    response = flask.send_from_directory('static', filename='js/serviceworker.js')
+    response = flask.send_from_directory('static', path='js/serviceworker.js')
     response.headers['Cache-Control'] = 'no-cache'
     return response
