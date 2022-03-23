64d0bdd19694c2b7c3f16046a77d5cfe12340c10
Rupus Reinefjord <rupus@reinefjord.net>
2022-03-23 20:36:07 +0100

    Make app.py executable and start dev server if executed
    And some related improvements

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from pathlib import Path
 
 from flasquelistan import factory
@@ -11,9 +12,7 @@ instance_config_name = 'config.py'
 path.joinpath(instance_dir_name).mkdir(exist_ok=True)
 instance_config = path.joinpath(instance_dir_name, instance_config_name)
 
-if not instance_config.is_file():
-    instance_config.touch()
-    instance_config.write_text("""\
+default_config = """\
 from pathlib import Path
 
 DEBUG = True
@@ -22,6 +21,13 @@ SECRET_KEY = 'not the default secret!'
 BASEDIR = Path(__file__).parent.resolve() # is now `instance/`
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASEDIR.joinpath('db.sqlite'))
-""")
+"""
+
+if not instance_config.is_file():
+    instance_config.touch()
+    instance_config.write_text(default_config)
 
 app = factory.create_app(instance_config='config.py')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- start_first_time.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export FLASK_APP=app.py
-sh update_translations.sh
-flask initdb
-flask populatetestdb
-flask createadmin
-./start_dev_server.sh
